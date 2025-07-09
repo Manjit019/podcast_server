3d7407f07f345124e92fe22d76379c140612c0f6
Manjit <manjitkumar1725@gmail.com>
2025-07-09 16:18:39 +0530

    .

--- keystone.ts
+++ keystone.ts
@@ -1,10 +1,9 @@
-
-import { config } from '@keystone-6/core'
-import { withAuth, session } from './auth'
-import { User } from './schemas/user'
-import { Podcast } from './schemas/podcast';
-import { Artist } from './schemas/artist';
-import { extendGraphqlSchemas } from './schemas/extend';
+import { config } from "@keystone-6/core";
+import { withAuth, session } from "./auth";
+import { User } from "./schemas/user";
+import { Podcast } from "./schemas/podcast";
+import { Artist } from "./schemas/artist";
+import { extendGraphqlSchemas } from "./schemas/extend";
 
 export default withAuth(
   config({
@@ -12,18 +11,22 @@ export default withAuth(
       // we're using sqlite for the fastest startup experience
       //   for more information on what database might be appropriate for you
       //   see https://keystonejs.com/docs/guides/choosing-a-database#title
-      provider: 'sqlite',
+      provider: "sqlite",
       url: 'file:./db.sqlite',
-    },
-    lists : {User,Podcast,Artist},
-    session,
-    ui : {
-      isAccessAllowed : ({session}) => {
-        return !!session?.data?.isAdmin;
+      onConnect: async (context) => {
+        console.log("Connected to DB.");
       }
     },
-    graphql : {
-      extendGraphqlSchema : extendGraphqlSchemas,
-    }
+    
+    lists: { User, Podcast, Artist },
+    session,
+    ui: {
+      isAccessAllowed: ({ session }) => {
+        return !!session?.data?.isAdmin;
+      },
+    },
+    graphql: {
+      extendGraphqlSchema: extendGraphqlSchemas,
+    },
   })
-)
+);
